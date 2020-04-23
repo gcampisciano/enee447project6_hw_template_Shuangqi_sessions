2a6dde692dabac407198c65f539848c105c4148f
gcampisciano <42872053+gcampisciano@users.noreply.github.com>
2020-04-23 18:32:52 -0400

    Update trap_handlers.c

--- trap_handlers.c
+++ trap_handlers.c
@@ -104,6 +104,13 @@ trap_handler(unsigned long r0, unsigned long r1, unsigned long r2)
 			dump_processes();
 			return 0;
 			break;
+		case SYSCALL_GET_CORE_ID:
+			log("SYSCALL_GET_CORE_ID device =", r0);
+			if(r0 < MAX_DEVICES) {
+				devtab[r0].read();
+			}
+			return 0;
+			break;
 		case SYSCALL_DUMP_RANGE:
 			log("SYSCALL_DUMP_RANGE start =", r0);
 			log("SYSCALL_DUMP_RANGE end   =", r1);
